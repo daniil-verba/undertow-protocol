5854f685a0e3c74db7eb6fc8a4cd3362d6602f85
Daniil Verba <daniilverba123@gmail.com>
2026-07-17 17:31:52 +0700

    fix(beacon): export beacon_client.rs in lib.rs

---
+++ src/beacon/mod.rs
@@ -0,0 +1,7 @@
+//! ## Beacon Module / Модуль маяка
+//!
+//! Beacon client for connecting to beacon servers.
+//! / Клиент маяка для подключения к серверам маяка.
+
+pub mod beacon_client;
+pub use beacon_client::BeaconClient;
